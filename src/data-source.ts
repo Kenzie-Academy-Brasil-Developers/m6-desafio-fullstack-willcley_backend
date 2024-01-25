33cf496c6335f710c1bdf1296cd4349a2a80f4fb
Willcley Melo <willcley2003@gmail.com>
2024-01-25 16:02:44 -0300

    feat: initial structure

---
+++ src/data-source.ts
@@ -0,0 +1,23 @@
+import path from "path";
+import { DataSource, DataSourceOptions } from "typeorm";
+import "dotenv/config";
+
+const DataSourceConfig = (): DataSourceOptions => {
+    const entitiesPath = path.join(__dirname, "./entities/**.{js, ts}");
+    const migrationsPath = path.join(__dirname, "./migrations/**.{js, ts}");
+
+    if (!process.env.DATABASE_URL) {
+        throw new Error("Env var DATABASE_URL does not exists.");
+    };
+
+    return {
+        type: "postgres",
+        url: process.env.DATABASE_URL,
+        synchronize: false,
+        logging: true,
+        entities: [entitiesPath],
+        migrations: [migrationsPath]
+    };
+};
+
+export const AppDataSource = new DataSource(DataSourceConfig());
