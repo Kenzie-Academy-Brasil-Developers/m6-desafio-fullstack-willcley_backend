c9447401e605e462b8269e7f957746dd2524ee4d
Willcley Melo <willcley2003@gmail.com>
2024-01-29 20:01:32 -0300

    fix: fixed code

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,11 @@ import "reflect-metadata";
 import "express-async-errors";
 import express, { Application } from "express";
 import { handleError } from "./middlewares/handleErrors.middleware";
+import { routers } from "./routers";
 
 export const app: Application = express();
 app.use(express.json());
+
+app.use("/", routers);
 
 app.use(handleError);
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,16 @@
-import { app } from "./app";
 import "dotenv/config";
+import { app } from "./app";
 import { AppDataSource } from "./data-source";
 
 AppDataSource.initialize()
-.then(() => {
-    console.log("database is running");
-    const PORT = process.env.PORT || 3000;
-    app.listen(PORT, () => {
-        console.log(`Server is running on http://localhost:${PORT}`)
+    .then(() => {
+        console.log("Server is running");
+
+        const PORT: number = Number(process.env.PORT) || 3000;
+        app.listen(PORT, () => {
+            console.log(`Server is running on http://localhost:${PORT}`)
+        });
+    })
+    .catch((err: unknown): void => {
+        console.log("Error during Data Source initialization", err);
     });
-});
