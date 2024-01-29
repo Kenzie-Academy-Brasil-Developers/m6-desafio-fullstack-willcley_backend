5b33b84f63a1d2935a3a8618714300f05b180e6b
Willcley Melo <willcley2003@gmail.com>
2024-01-29 19:52:11 -0300

    feat: create middlewares

---
+++ src/middlewares/clients.middleware.ts
@@ -0,0 +1,41 @@
+import AppError from "../errors/AppErrors.error";
+import { NextFunction, Request, Response } from "express";
+import { Client, Contact } from "../entities/index";
+import { clientRepo } from "../repositories";
+import { clientReturnSchema } from "../schemas/clients.schemas";
+
+export const verifyClientExists = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+): Promise<void> => {
+    let { clientId } = req.params;
+    if (req.body.clientId) clientId = req.body.clientId;
+
+    const client: Client | null = await clientRepo.findOne({
+        where: { id: clientId },
+        relations: { emails: true, contacts: true },
+    });
+
+    if (!client) throw new AppError("Client not found.", 404);
+
+    const clientReturn = clientReturnSchema.parse(client);
+    res.locals.client = clientReturn;
+
+    return next();
+};
+
+export const verifyClientEmailOwner = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+): Promise<void> => {
+    const { client, email } = res.locals;
+
+    if (email.clientId != client.id) throw new AppError(
+        "You can not update this email.",
+        401
+    );
+
+    return next();
+};
